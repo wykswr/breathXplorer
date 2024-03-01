9962a60ea92682e3eac7959140e0567d69e38b0b
wangyk <wangyukai159@outlook.com>
2024-03-01 11:22:40 -0800

    adduct list

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirement.txt") as f:
 
 setup(
     name="breathXplorer",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     install_requires=required_packages,
     author="wykswr",
