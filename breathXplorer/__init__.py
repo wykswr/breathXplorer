5f839c636fbbded97f92731befe78109ae574097
wangyk <wangyukai159@outlook.com>
2024-03-01 14:18:38 -0800

    reproducibility

--- breathXplorer/__init__.py
+++ breathXplorer/__init__.py
@@ -1,2 +1,14 @@
+# fix random seed for reproducibility
+from random import seed as rseed
+rseed(114514)
+from numpy.random import seed
+seed(114514)
+
+import warnings
+
+# Ignore FutureWarning globally
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
 from .extract import find_feature, merge_result
 from .file_io import retrieve_tandem
