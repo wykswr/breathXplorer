14586052ea5c821c98988d7837bf6a041144e19f
wangyk <wangyukai159@outlook.com>
2024-05-08 16:48:01 -0700

    intensity control

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirement.txt") as f:
 
 setup(
     name="breathXplorer",
-    version="0.2.5",
+    version="0.2.6",
     packages=find_packages(),
     install_requires=required_packages,
     author="wykswr",
